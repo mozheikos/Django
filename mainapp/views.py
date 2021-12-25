73db76a86292199f2bcc26f2c8617cda5a7cc808
mozheikos <mozheiko.stanislav@yandex.ru>
2021-12-25 12:43:18 +0300

    Mozheiko Stanislav Lesson_2 homework repeat, filters, load data from HDD

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,37 +1,37 @@
 from django.shortcuts import render
 import datetime
+import json
+
+
+def get_controller_data(file_name):
+    with open(f'data_json/{file_name}', 'r') as f:
+        data = json.load(f)
+    return data
 
 
 def main(request):
     title = 'Главная'
-    products = [
-        {
-            'name': "Отличный стул",
-            'desc': "Расположись комфортно",
-            'image_src': "product-1.jpg",
-            'image_href': "/product/1/",
-            'alt': "стул 1"
-        },
-        {
-            'name': "Еще один хороший стул",
-            'desc': "Вставать не захочешь",
-            'image_src': "product-2.jpg",
-            'image_href': "/product/2/",
-            'alt': "стул 2"
-        }
-    ]
+
+    products = get_controller_data('main_products.json')
     content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
     title = "продукты"
-    content = {"title": title}
+
+    links = get_controller_data('links.json')
+    same_products = get_controller_data('same_products.json')
+    content = {"title": title, "links": links, "same_products": same_products}
     return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
     title = "о нас"
-    visit_date = datetime.datetime.now()
-    content = {"title": title, "visit_date": visit_date}
+
+    contact_cards = get_controller_data('cities.json')
+    today = datetime.datetime.now()
+
+    content = {"title": title, "visit_date": today,
+               'contact_cards': contact_cards}
     return render(request, "mainapp/contact.html", content)
