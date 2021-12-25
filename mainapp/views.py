2e2c119d14d8a53bdefe5fb17e5570d7cd251394
mozheikos <mozheiko.stanislav@yandex.ru>
2021-12-25 09:42:52 +0300

    Mozheiko_Stanislav_homework_Lesson_2. Context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+import datetime
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+    products = [
+        {
+            'name': "Отличный стул",
+            'desc': "Расположись комфортно",
+            'image_src': "product-1.jpg",
+            'image_href': "/product/1/",
+            'alt': "стул 1"
+        },
+        {
+            'name': "Еще один хороший стул",
+            'desc': "Вставать не захочешь",
+            'image_src': "product-2.jpg",
+            'image_href': "/product/2/",
+            'alt': "стул 2"
+        }
+    ]
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = "продукты"
+    content = {"title": title}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = "о нас"
+    visit_date = datetime.datetime.now()
+    content = {"title": title, "visit_date": visit_date}
+    return render(request, "mainapp/contact.html", content)
