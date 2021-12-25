b18f39f7e85d61a4f50793fb2086501ca49fae3c
mozheikos <mozheiko.stanislav@yandex.ru>
2021-12-25 08:46:05 +0300

    Mozheiko_Stanislav_homework_Lesson_2. Templates

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -5,8 +5,7 @@ import mainapp.views as mainapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp.main),
-    path("index.html", mainapp.main),
-    path("products.html", mainapp.products),
-    path("contact.html", mainapp.contact),
+    path('', mainapp.main, name='main'),
+    path("products/", mainapp.products, name='products'),
+    path("contact/", mainapp.contact, name='contact'),
 ]
