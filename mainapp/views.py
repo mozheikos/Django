73cc9cc866b7b81c1c2d6cb38f34849689fd9a69
mozheikos <85384650+mozheikos@users.noreply.github.com>
2021-12-25 14:21:55 +0300

    Mozheiko_Stanislav_Lesson 2_homework (#4)
    * Mozheiko_Stanislav_homework_Lesson_2. Templates
    
    * Mozheiko_Stanislav_homework_Lesson_2. Context
    
    * Mozheiko Stanislav Lesson_2 homework repeat, filters, load data from HDD

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
+import datetime
+import json
+
+
+def get_controller_data(file_name):
+    with open(f'data_json/{file_name}', 'r') as f:
+        data = json.load(f)
+    return data
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    title = 'Главная'
+
+    products = get_controller_data('main_products.json')
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = "продукты"
+
+    links = get_controller_data('links.json')
+    same_products = get_controller_data('same_products.json')
+    content = {"title": title, "links": links, "same_products": same_products}
+    return render(request, "mainapp/products.html", content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = "о нас"
+
+    contact_cards = get_controller_data('cities.json')
+    today = datetime.datetime.now()
+
+    content = {"title": title, "visit_date": today,
+               'contact_cards': contact_cards}
+    return render(request, "mainapp/contact.html", content)
