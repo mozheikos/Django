751890ee83b225a4e58e5f7b4f1b2f67cbce2700
mozheikos <mozheiko.stanislav@yandex.ru>
2022-01-02 18:48:22 +0300

    Lesson_4 homework + validator + default=20 in age field of ShopUser

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -21,17 +21,24 @@ class ShopUserEditForm(UserChangeForm):
         for field in self.fields.values():
             field.widget.attrs["class"] = "form-control"
 
-    """def clean_avatar(self):
+    """Валидатор картинки. self - объект из формы, cleaned_data - содержимое 
+    формы. Оттуда достаем поле аватар - название файла с расширением. Отрезаем
+    расширение и проверяем, относится ли оно к поддерживаемым типам. Если да - 
+    возвращаем путь: каталог + имя файла, если нет - возвращаем None"""
+
+    def clean_avatar(self):
         allowed_types = ['jpg', 'jpeg', 'png', 'svg', 'bmp']
         ava = self.cleaned_data['avatar']
         if ava:
-            ava = ava.rsplit('.', 1)
+            ava = str(ava).rsplit('.', 1)
             if not ava[1] or ava[1] not in allowed_types:
-                raise forms.ValidationError('Не поддерживаемый тип файла')"""
+                raise forms.ValidationError('Не поддерживаемый тип файла')
+        return 'users_avatars/' + '.'.join(ava) if ava else None
 
     class Meta:
         model = ShopUser
-        fields = ("username", "first_name", "last_name", "email", "age", "avatar")
+        fields = ("username", "first_name",
+                  "last_name", "email", "age", "avatar")
 
 
 class ShopUserRegisterForm(UserCreationForm):
