6e36bc4fe80e014f67ec51921b3446e3ea2d0d5a
mozheikos <mozheiko.stanislav@yandex.ru>
2022-01-14 06:34:49 +0300

    Squashed commit of the following:
    commit f9360b69f260c043006cbf51eb29015c755e9687
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Fri Jan 14 06:22:10 2022 +0300
    
        lesson 5.
    
    commit affe3fb63052d0eb0a3421ba63eb123cf0364a70
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Wed Jan 12 22:06:03 2022 +0300
    
        lesson_5 now exactly all
    
    commit 70a6cb00ac7da81bd092d2d7f442abd5c931ee03
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Wed Jan 12 22:03:52 2022 +0300
    
        lesson_5 links works + basket works + task 7 done + fixed mistake when unregistered
    
    commit 615eba9afbc345e6df61a86ff9248e4e55ab03ae
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Wed Jan 12 21:53:14 2022 +0300
    
        lesson_5 links works + basket works + task 7 done cd /home/stanislav/djangoBasics/geekshop ; /usr/bin/env /home/stanislav/djangoBasics/venv/bin/python3.8 /home/stanislav/.vscode-server/extensions/ms-python.python-2021.12.1559732655/pythonFiles/lib/python/debugpy/launcher 41789 -- /home/stanislav/djangoBasics/geekshop/manage.py runserver 0.0.0.0:8000
    
    commit 33aa9aa1fc4585293fb9dd83de98c6e644a18f0a
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Wed Jan 12 21:04:31 2022 +0300
    
        Lesson 5. Code style fix
    
    commit 4b4e7694e492ebd875fd91ffe5162215d8dc523c
    Author: mozheikos <mozheiko.stanislav@yandex.ru>
    Date:   Tue Jan 11 21:51:42 2022 +0300
    
        lesson_5 basket.model + add.view + products.template to add
    
    Lesson_5

---
+++ basketapp/models.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+from mainapp.models import Product
+
+
+class Basket(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(verbose_name="дата добавления", auto_now_add=True)
+
+    @property
+    def product_cost(self):
+        return self.quantity * self.product.price
+
+    @classmethod
+    def product_count(cls, user):
+        products = cls.objects.filter(user=user)
+        cls._count = 0
+        for item in products:
+            cls._count += item.quantity
+        return cls._count
+
+    @classmethod
+    def total_cost(cls, user):
+        products = cls.objects.filter(user=user)
+        cls._total_cost = 0
+        for item in products:
+            cls._total_cost += item.product_cost
+        return cls._total_cost
---
+++ basketapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+import basketapp.views as basketapp
+
+from .apps import BasketappConfig
+
+app_name = BasketappConfig.name
+
+urlpatterns = [
+    path("", basketapp.basket, name="view"),
+    path("add/<int:pk>", basketapp.add_to_basket, name="add"),
+    path("remove/<int:pk>", basketapp.remove_from_basket, name="remove"),
+]
