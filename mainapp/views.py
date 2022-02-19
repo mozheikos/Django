84d77554e5a5ccaef39f04eaf40abae5e84a5d25
mozheikos <mozheiko.stanislav@yandex.ru>
2022-02-19 14:37:08 +0300

    Lesson 15: low_cache with memcached

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 import json
+from os import link
 from random import choice
-
+from django.core.cache import cache
 from django.conf import settings
 from django.core.paginator import Paginator
 from django.shortcuts import render
@@ -8,25 +9,71 @@ from django.utils import timezone
 from .models import Category, Contact, Product
 
 
-class Fact():
-    def get_factorial(self):
-        fact = 1
-        for i in range(1, 15001):
-            fact *= i
-        return fact
+def get_links():
+    if settings.LOW_CACHE:
+        key = "menu_links"
+        links = cache.get(key)
+        if links is None:
+            links = Category.objects.filter(is_active=True).order_by("id")
+            cache.set(key, links)
+        return links
+    else:
+        return Category.objects.filter(is_active=True).order_by("id")
 
 
-def get_all_active_products(category_pk=1):
-    if category_pk == 1:
-        category = Category.objects.filter(id__gt=1, is_active=True)
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f"product_{pk}"
+        product = cache.get(key)
+        if product is None:
+            product = Product.objects.get(pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return Product.objects.get(pk=pk)
+
+
+def get_all_active_products(*args):
+    if not len(args):
+        order = ("id", )
+    else:
+        order = args
+    if settings.LOW_CACHE:
+        key = "all_active_products"
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(
+                is_active=True, category__is_active=True).order_by(*order)
+            cache.set(key, products)
+        return products
+    else:
+        category = Category.objects.filter(
+            id__gt=1, is_active=True).order_by("id")
         products = []
         for item in category:
-            _products = item.category_products
+            _products = item.category_products.order_by(*order)
             products.extend(_products)
+        return products
+
+
+def get_category_products(category_pk, *args):
+    if not len(args):
+        order = ("id", )
+    else:
+        order = args
+
+    if settings.LOW_CACHE:
+        key = f"products_category_{category_pk}"
+        products = cache.get(key)
+        if products is None:
+            category = Category.objects.get(pk=category_pk)
+            products = category.category_products.order_by(*order)
+            cache.set(key, products)
+        return products
     else:
         category = Category.objects.get(pk=category_pk)
-        products = category.category_products
-    return products
+        products = category.category_products.order_by(*order)
+        return products
 
 
 def get_controller_data(file_name):
@@ -36,29 +83,20 @@ def get_controller_data(file_name):
 
 
 def get_random_product():
-    #products = Product.objects.filter(is_active=True, category__is_active=True)
     products = get_all_active_products()
     product = choice(products)
     return (product, [x for x in products if x.category_id == product.category_id and x.id != product.id])
-    # return (product, products.filter(category_id=product.category_id).exclude(pk=product.pk))
 
 
 def main(request):
     title = "Главная"
 
-    products = get_all_active_products()
-    #category = Category.objects.filter(id__gt=1, is_active=True)
-    #products = []
-    # for item in category:
-    #    _products = item.category_products
-    #    products.extend(_products)
-    #products = Product.objects.filter(is_active=True, category__is_active=True)
+    products = get_all_active_products("category_id", "name")
 
     content = {
         "title": title,
         "products": products,
         "media_url": settings.MEDIA_URL,
-        "fact": Fact(),
     }
     return render(request, "mainapp/index.html", content)
 
@@ -67,33 +105,24 @@ def products(request, product_pk=None, category_pk=0, page=1):
     category_pk = int(category_pk)
     page = int(page)
     title = "продукты"
-    links = Category.objects.filter(is_active=True).order_by("id")
+    links = get_links()
     hot = False
     product_large = None
     if product_pk:
         product_pk = int(product_pk)
-        product_large = Product.objects.get(pk=product_pk)
-        same_products = get_all_active_products(
-            product_large.category_id).exclude(pk=product_pk)
-        # same_products = Product.objects.filter(category_id=product_large.category_id, is_active=True).exclude(
-        #     pk=product_pk
-        # )
+        product_large = get_product(product_pk)
+        same_products = get_category_products(
+            product_large.category_id, 'price').exclude(pk=product_pk)
     else:
         if not category_pk:
             product_large, same_products = get_random_product()
             category_pk = product_large.category_id
-            # same_products = Product.objects.filter(category_id=product_large.category_id, is_active=True).exclude(
-            #    pk=product_large.pk
-            # )
             hot = True
         else:
-            same_products = get_all_active_products(category_pk=category_pk)
-            # if category_pk == 1:
-            #     same_products = Product.objects.filter(
-            #         is_active=True, category__is_active=True).order_by("category_id")
-            # else:
-            #     same_products = Product.objects.filter(
-            #         category_id=category_pk, is_active=True)
+            if category_pk == 1:
+                same_products = get_all_active_products("category_id", "name")
+            else:
+                same_products = get_category_products(category_pk, "name")
 
     paginator = Paginator(same_products, 3)
     products_paginator = paginator.page(page)
@@ -107,8 +136,7 @@ def products(request, product_pk=None, category_pk=0, page=1):
         "category": category_pk,
         "hot": hot,
     }
-    # if category_pk:
-    #    print(f"User select category: {category_pk}")
+
     return render(request, "mainapp/products.html", content)
 
 
